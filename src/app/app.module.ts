7654d4fa55e3502ec7030fa35f04fbc9c8ad48de
unknown <ngocanh732001@gmail.com>
2022-08-28 18:36:08 +0700

    done the functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CheckboxComponent } from './component/checkbox/checkbox.component';
+import { LineChartComponent } from './component/line-chart/line-chart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgChartsModule],
+  declarations: [
+    AppComponent,
+    CheckboxComponent,
+    LineChartComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgChartsModule,
+    HttpClientModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataset } from 'chart.js';
+import { ResasService } from '../service/resas.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  chartLabels: string[] = [];
+  chartData: ChartDataset[] = [];
+  lstPrefectures: any[] = [];
+  checkedItem: any;
+
+  constructor(private resas: ResasService) {}
+
+  ngOnInit(): void {
+    this.getPrefectures();
+  }
+
+  randomColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
+  }
+  getChartLabels(res: any) {
+    this.chartLabels = [];
+    res.result.data[0].data.forEach((element: any) => {
+      this.chartLabels.push(element.year.toString());
+    });
+  }
+  getChartData(prefCode: number) {
+    this.resas.getPopulation(prefCode).subscribe((res) => {
+      this.getChartLabels(res);
+      res.result.data.forEach((element: any) => {
+        const color = '#' + this.randomColor();
+        const data: number[] = [];
+        element.data.forEach((item: any) => {
+          data.push(item.value);
+        });
+        this.chartData.push({
+          data: data,
+          label: element.label,
+          backgroundColor: 'rgba(0, 0, 0, 0.1)',
+          hoverBorderWidth: 10,
+          borderColor: color,
+          pointBackgroundColor: color,
+          pointHoverBorderColor: color,
+          tension: 0.2,
+        });
+      });
+      console.log(this.chartData);
+    });
+  }
+  getPrefectures() {
+    this.resas.getPrefectures().subscribe((res) => {
+      console.log(res);
+      this.lstPrefectures = res.result;
+    });
+  }
+  getCheckedItem(data: any) {
+    this.chartData = [];
+    this.checkedItem = data;
+    this.getChartData(data.prefCode);
+  }
+}
